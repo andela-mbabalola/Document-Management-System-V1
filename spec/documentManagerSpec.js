be3eace6ddaee2a508803251eae5158a0f738701
Maryam <maryamolabisi02@gmail.com>
2015-12-10 15:58:09 +0100

    feat(spec): add documentManagerSpec.js

---
+++ spec/documentManagerSpec.js
@@ -0,0 +1,18 @@
+/****************************************************************
+*Name: String Class Test.
+*Description: To test the String Class program's functionality
+*Author: Babalola Maryam
+*****************************************************************/
+describe ("Document Management System", function () {
+  describe ("User", function () {
+
+  });
+
+  describe ("Documents", function () {
+
+  });
+
+  describe ("Role", function () {
+
+  });
+});
